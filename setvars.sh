21a3d6d90e6a44c822aebd7df00ae06094d81120
thoriumrobot <ks225@njit.edu>
2025-03-04 14:31:56 -0500

    args

---
+++ setvars.sh
@@ -0,0 +1,3 @@
+export ANDROID_HOME=/home/somi/artifact/android-sdk
+export PATH=/home/somi/artifact/checker-framework-3.42.0/checker/bin:/home/somi/artifact/infer-linux64-v1.1.0/bin/:/home/somi/artifact/android-sdk/tools/bin/:$PATH
+export CHECKERFRAMEWORK=/home/somi/artifact/checker-framework-3.42.0
